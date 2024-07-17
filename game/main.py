d2f9fc6c5866fddae871a4b8f70d1374e2389852
Lisandro1234 <lisiibayon@gmail.com>
2024-07-17 09:31:22 +0200

    save game

--- game/main.py
+++ game/main.py
@@ -2,31 +2,21 @@ import random
 
 
 
-cpu_winers = 0
-usuario_winers = 0
-
-
-
-rounds = 1
-
-
-while True:
+def e_opcion():
     
-    print("*"*10)
-    print("ROUND", rounds)
-    print("*"*10)
-    
-
+    opcion = ("piedra", "papel", "tijera")
     usuario = input("piedra, papel o tijera: ")
     usuario = usuario.lower()
-    opcion = ("piedra", "papel", "tijera")
+    if not usuario in opcion:
+        print("vuelve a intentarlo")
+        return None, None
+    
     cpu_opcion = random.choice(opcion)
     print("opcion de cpu: ", cpu_opcion)
+    return usuario, cpu_opcion
 
-    if usuario != opcion:
-        print("vuelve a intentarlo")
-        continue
 
+def partida(usuario, cpu_opcion, usuario_winers, cpu_winers):
     if usuario == cpu_opcion:
         print("Empate")
     elif usuario == "piedra" and cpu_opcion == "papel":
@@ -47,15 +37,37 @@ while True:
     elif usuario == "tijera" and cpu_opcion == "papel":
         print("usuario gana")
         usuario_winers +=1
-         
-    
-    
+    return usuario_winers, cpu_winers
+
+
+def ganador(usuario_winers, cpu_winers):
     if usuario_winers == 3:
         print("USUARIO GANA LA PARTIDA")
-        break
+        exit()
     elif cpu_winers == 3:
         print("CPU GANA LA PARTIDA")
-        break
-    
-    rounds += 1    
+        exit()
         
+    
+
+def run_game():
+    usuario_winers = 0
+    cpu_winers = 0
+    rounds = 1
+    
+    while True:
+    
+        print("*"*10)
+        print("ROUND", rounds)
+        print("*"*10)
+    
+        rounds += 1
+
+        usuario, cpu_opcion = e_opcion()
+        usuario_winers, cpu_winers = partida(usuario, cpu_opcion, usuario_winers, cpu_winers)
+        ganador(usuario_winers, cpu_winers)
+
+    
+        
+           
+run_game()
